21d69511e4f72686b61b31d843fa43ada5402523
alphaione <93822183+alphaione@users.noreply.github.com>
2025-01-17 11:44:42 +0800

    Add files via upload

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -8,6 +8,8 @@
 
 # ------------------------------- Main source started -------------------------------
 #
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-argon）
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
@@ -25,6 +27,13 @@ echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_rele
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+#
+# Add luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone https://gitclone.com/github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#Add luci-app-partexp
+rm -rf package/luci-app-partexp
+git clone https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
